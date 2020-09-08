0c0cc87ab9581d7ff45bf8329aec9943016749e8
DattatrayPatil73076 <dattatray.patil.733076@gmail.com>
2020-09-08 15:29:48 +0530

    create products

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -2,8 +2,39 @@ pragma solidity ^0.5.10;
 
 contract Marketplace {
     string public name;
+    uint public productCount = 0;
+    mapping(uint => Product) public products;
+
+    struct Product{
+    uint id;
+    string name;
+    uint price;
+    address owner;
+    bool purchased;
+    }
+
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
 
     constructor() public {
         name = "Dapp University Marketplace";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        // Require a valid name
+        //require(bytes(_name).lenght > 0);
+        // Require a valid price
+        require(_price > 0);
+        // Increment product productCount
+        productCount ++;
+        // Create the product
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        // Trigger on event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,6 +1,7 @@
 const Marketplace = artifacts.require('Marketplace')
 
-contract(Marketplace, (accounts) => {
+contract(Marketplace, ([deployer, seller, buyer]) => {
+  //accounts
   let marketplace
 
   before(async () => {
@@ -19,6 +20,37 @@ contract(Marketplace, (accounts) => {
     it('it has name', async () => {
       const name = await marketplace.name()
       assert.equal(name, 'Dapp University Marketplace')
+    })
+  })
+
+  describe('product', async () => {
+    let result, productCount
+
+    before(async () => {
+      result = await marketplace.createProduct(
+        'Iphone X',
+        web3.utils.toWei('1', 'Ether'),
+        { from: seller },
+      )
+      productCount = await marketplace.productCount()
+    })
+
+    it('creates product', async () => {
+      //SUCCESS
+      assert.equal(productCount, 1)
+      //console.log(result.logs)
+      const event = result.logs[0].args
+      assert.equal(
+        event.id.toNumber(),
+        productCount.toNumber(),
+        'id is correct',
+      )
+      assert.equal(event.name, 'Iphone X', 'name is correct')
+      assert.equal(event.price, '1000000000000000000', 'price is correct')
+      assert.equal(event.owner, seller, 'owner is correct')
+      assert.equal(event.purchased, false, 'purchased is correct')
+
+      //FAILURES
     })
   })
 })
